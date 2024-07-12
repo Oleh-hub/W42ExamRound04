484b0340b3ba4254b273c61cc87f4df2dedd78e3
Oleh Ruban <oruban@c2r2s2.42wolfsburg.de>
2024-07-12 18:03:49 +0200

    one more test

--- tests/microshell1.c
+++ tests/microshell1.c
@@ -1,17 +1,23 @@
+
+		// for (int j = 0; envp[j]; j++)
+		// 	printf("%s\n", envp[j]);
+		// printf("%d\n", i);
 // #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
 
 int err(char *str)
 {
-	while(*str)
+	while (*str)
 		write(2, str++, 1);
 	return 1;
 }
 
 int cd(char **argv, int i)
 {
+	(void) argv;
+
 	if (i != 2)
 		return err("error: cd: bad arguments\n");
 	if (chdir(argv[1]) == -1)
@@ -19,54 +25,50 @@ int cd(char **argv, int i)
 	return 0;
 }
 
-int exec(char **argv, int i, char **envp)
+int	exec(char **argv, int i, char **envp)
 {
-	(void) envp;
-	int status = 0;
 	int has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int status = 0;
 	int fd[2];
 
 	if (!has_pipe && !strcmp(argv[0], "cd"))
 		return cd(argv, i);
+
 	if (has_pipe && pipe(fd) == -1)
 		return err("error: fatal\n");
 	
 	int pid = fork();
-	if (pid == -1)
-		return err("error: fatal\n");
 	if (!pid)
 	{
-		argv[i] = 0;
-		if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return err("error: fatal\n");
-		// printf("DEBUG\n");
-		if ( !strcmp(argv[0], "cd"))
-			return cd(argv, i);
-		// printf("%s\n", *argv);
-		// printf("%s\n", *envp);
-		execve(*argv, argv, envp);
+		if (has_pipe && !strcmp(argv[0], "cd"))
+		return cd(argv, i);
+		argv[i] = 0;
+		execve(argv[0], argv, envp);
 		return err("error: cannot execute "), err(argv[0]), err("\n");
 	}
 	waitpid(pid, &status, 0);
-	if(has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		return err("error: fatal\n");
-	// printf("%d", WIFEXITED(status) && WEXITSTATUS(status));
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	int i = 0;
-	int status = 0;
+	int		status = 0;
+	int 	i = 0;
+
 	if (argc > 1)
 	{
-		while (argv[i] && argv[++i]) // ++i
+		while (argv[i] && argv[++i])
 		{
 			argv += i;
 			i = 0;
 			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
 				i++;
-			status = exec(argv, i, envp);
+			if (i)
+				exec(argv, i, envp);
 		}
 	}
 	return status;
