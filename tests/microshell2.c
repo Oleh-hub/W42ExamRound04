273e7aaaee13409deb0cda1cfc83502780d02824
Oleh Ruban <oruban@c2r2s2.42wolfsburg.de>
2024-07-13 20:18:23 +0200

    1 mnore training microshell exam04 in W42

--- tests/microshell2.c
+++ tests/microshell2.c
@@ -1,76 +1,73 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
 int err(char *str)
 {
-	while (*str)
+	while(*str)
 		write(2, str++, 1);
 	return 1;
 }
 
-int cd (char **argv, int i)
+int cd(char **argv, int i)
 {
 	if (i != 2)
 		return err("error: cd: bad arguments\n");
 	if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory "), err(argv[1]), err("\n");
+		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
 	return 0;
 }
 
 int exec(char **argv, int i, char **envp)
 {
-	// for (int j = 0; j <= i; j++)
-	// 	printf("%s ", *(argv + j));
-	// printf ("%d\n", i);
-
+	(void) envp;
 	int status = 0;
-	int fd[2];
-	int pid;
 	int has_pipe = argv[i] && !strcmp(argv[i], "|");
-	
+	int fd[2];
+
 	if (!has_pipe && !strcmp(argv[0], "cd"))
 		return cd(argv, i);
-	
 	if (has_pipe && pipe(fd) == -1)
 		return err("error: fatal\n");
-		
-	pid = fork();
+	
+	int pid = fork();
 	if (pid == -1)
 		return err("error: fatal\n");
 	if (!pid)
 	{
 		argv[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[1]) == -1 || close (fd[0]) == -1))
+		if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return err("error: fatal\n");
-		if (!strcmp(argv[0], "cd"))
+		// printf("DEBUG\n");
+		if ( !strcmp(argv[0], "cd"))
 			return cd(argv, i);
+		// printf("%s\n", *argv);
+		// printf("%s\n", *envp);
 		execve(*argv, argv, envp);
-		return err("error: cannot execute "), err(*argv), err("\n");
+		return err("error: cannot execute "), err(argv[0]), err("\n");
 	}
 	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[1]) == -1 || close (fd[0]) == -1))
+	if(has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		return err("error: fatal\n");
+	// printf("%d", WIFEXITED(status) && WEXITSTATUS(status));
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	int status = 0;
 	int i = 0;
-
-	if (argc < 2)
-		return status;
-	
-	while (argv[i] && argv[++i])
+	int status = 0;
+	if (argc > 1)
 	{
-		argv += i;
-		i = 0;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-			i++;
-		if (i)
+		while (argv[i] && argv[++i]) // ++i
+		{
+			argv += i;
+			i = 0;
+			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+				i++;
 			status = exec(argv, i, envp);
+		}
 	}
 	return status;
 }
